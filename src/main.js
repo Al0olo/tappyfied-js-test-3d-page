fff5e889afaec33fd7490f5851371f6a143d5b2a
ali <straoid@gmail.com>
2024-03-22 04:56:52 +0200

    add main section

--- src/main.js
+++ src/main.js
@@ -9,11 +9,20 @@ import router from './router'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
+import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
 
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-layout class="rounded rounded-md">
+    
+    <v-main id="main" class="d-flex align-center justify-center" style="min-height: 300px;">
+      <div class="section-1">
+        <video autoplay muted loop id="myVideo">
+          <source src="https://apolix.global/wp-content/uploads/2023/02/Apolix-background.mp4" type="video/mp4">
+        </video>
+        <v-app-bar id="header" :elevation="0">
+          <template v-slot:prepend>
+              <img id="logo" src="../assets/logo.svg"/>
+          </template>
+        
+          <template v-slot:append>
+            <v-icon icon="mdi-heart"></v-icon>
+          
+            <v-btn icon="mdi-magnify"></v-btn>
+          
+            <v-btn icon="mdi-dots-vertical"></v-btn>
+          </template>
+        </v-app-bar>
+        
+      </div>
+    </v-main>
+  </v-layout>
 </template>
+
+<style>
+#logo{
+  width:10rem;
+  margin:2rem;
+}
+#header{
+  background: transparent;
+  padding-top: 1rem;
+
+}
+#myVideo{
+  position: absolute;
+  width: 100%;
+  left: 0;
+  top: 0;
+}
+</style>
 
 <script>
 // @ is an alias to /src
